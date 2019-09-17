7cfa506d3e4d5ad4f4ee689bc263eb352dc84618
Anwesh43 <anweshthecool0@gmail.com>
2019-09-17 23:10:07 +0530

    adding HalfBiArcFillView class

--- halfbiarcfillview/src/main/java/com/anwesh/uiprojects/halfbiarcfillview/HalfBiArcFillView.kt
+++ halfbiarcfillview/src/main/java/com/anwesh/uiprojects/halfbiarcfillview/HalfBiArcFillView.kt
@@ -60,3 +60,20 @@ fun Canvas.drawHBAFNode(i : Int, scale : Float, paint : Paint) {
     restore()
 }
 
+class HalfBiArcFillView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
